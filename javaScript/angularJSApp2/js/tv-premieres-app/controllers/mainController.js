e8cfc9d164279291758f79bcc9bb062e6689332f
mrmrbailey <mr.m.r.bailey@gmail.com>
2014-02-10 15:36:00 +0000

    js examples
    ajax and angular-js samples

---
+++ javaScript/angularJSApp2/js/tv-premieres-app/controllers/mainController.js
@@ -0,0 +1,14 @@
+app.controller("mainController", function($scope, $http){
+
+  //anjularJSDemo654 is username
+  //angularjsdemo is Password
+  //b8d5429bbcf8cdae981239ec6e80731d is key
+  
+  $scope.apiKey
+
+    $scope.init = function() {
+
+    };
+
+});
+
