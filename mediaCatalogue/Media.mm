27908ad5676adba83b2339752062a66a3b717bf9
mrmrbailey <mr.m.r.bailey@gmail.com>
2014-02-10 15:37:40 +0000

    mind map

---
+++ mediaCatalogue/Media.mm
@@ -0,0 +1,85 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Media" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1360060218576"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<node TEXT="books" POSITION="right" ID="ID_1307761776" CREATED="1360060221344" MODIFIED="1360065477027">
+<edge COLOR="#ff0000"/>
+<node TEXT="physical books" ID="ID_496750531" CREATED="1360060266701" MODIFIED="1360060274570">
+<node TEXT="from GoodReads" ID="ID_701188787" CREATED="1360060276541" MODIFIED="1360060282322"/>
+</node>
+<node TEXT="eBooks" ID="ID_1383162782" CREATED="1360060284195" MODIFIED="1360060289682">
+<node TEXT="from Calibre" ID="ID_1129386076" CREATED="1360060290787" MODIFIED="1360060296665"/>
+</node>
+</node>
+<node TEXT="films" POSITION="left" ID="ID_1620840640" CREATED="1360060228720" MODIFIED="1360065485748">
+<edge COLOR="#0000ff"/>
+<node TEXT="DVD&apos;s" ID="ID_1239082380" CREATED="1360060351763" MODIFIED="1360060355576">
+<node TEXT="unsure where from?&#xa;Manually Catalogue" ID="ID_968188607" CREATED="1360060373890" MODIFIED="1360060375851"/>
+</node>
+<node TEXT="IMDB" ID="ID_275490633" CREATED="1360060357425" MODIFIED="1360060364720">
+<node TEXT="from IMDB" ID="ID_321426647" CREATED="1360060376234" MODIFIED="1360060413074"/>
+</node>
+</node>
+<node TEXT="music" POSITION="right" ID="ID_1085129313" CREATED="1360060262357" MODIFIED="1360065482287">
+<edge COLOR="#00ff00"/>
+<node TEXT="CD&apos;s" ID="ID_1136319408" CREATED="1360060298786" MODIFIED="1360060302105">
+<node TEXT="unsure where from?&#xa;Manually Catalogue" ID="ID_841256407" CREATED="1360060314146" MODIFIED="1360060335496"/>
+</node>
+<node TEXT="Digital" ID="ID_1761706859" CREATED="1360060303339" MODIFIED="1360060312448">
+<node TEXT="from iTunes" ID="ID_1481187391" CREATED="1360060340075" MODIFIED="1360060346064"/>
+</node>
+</node>
+<node TEXT="TV" POSITION="left" ID="ID_1656591588" CREATED="1360065489695" MODIFIED="1360065493372">
+<edge COLOR="#ff00ff"/>
+<node TEXT="from IMDB" ID="ID_372330662" CREATED="1360065497773" MODIFIED="1360065501748"/>
+</node>
+</node>
+</map>
