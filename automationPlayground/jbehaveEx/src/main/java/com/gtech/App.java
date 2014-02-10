6f1e2b8cbd90c37f87186d65e8e503d38605ca03
mrmrbailey <mr.m.r.bailey@gmail.com>
2014-02-10 12:17:12 +0000

    recheck in automation
    following gitIgnore update rechecking in automation

---
+++ automationPlayground/jbehaveEx/src/main/java/com/gtech/App.java
@@ -0,0 +1,13 @@
+package com.gtech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
